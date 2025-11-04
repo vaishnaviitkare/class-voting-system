93fd079af88fd8d42437e12dbbc2d4412385a0f8
vaishnaviitkare <vaishuitkare@gmail.com>
2025-11-04 21:24:10 +0530

    one context is created to add delete operation and for votes data

---
+++ src/contextFiles/AD-context.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import React from "react";
+export const VoteContext=React.createContext();
+const ADprovider=(props)=>{
+const [votesData,setVotesData]=useState({
+Suresh:[],
+deepak:[],
+Abhi:[],
+});
+const addVote=(stdName,monitorName)=>{
+setVotesData((prev)=>({
+...prev,
+[monitorName]:[...prev[monitorName],stdName],
+}))
+};
+const deleteVote=(monitorName,stdName)=>{
+  setVotesData((prev)=>({
+     ...prev,
+     [monitorName]:prev[monitorName].filter((name)=>name!=stdName),
+  }))
+};
+
+return(
+<VoteContext.Provider value={{votesData,addVote,deleteVote}}>
+{props.children}
+</VoteContext.Provider>
+)
+}
