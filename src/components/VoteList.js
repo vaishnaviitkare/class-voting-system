e61d56ee97c787e930acab02cd76161cb64c9c2f
vaishnaviitkare <vaishuitkare@gmail.com>
2025-11-06 16:40:40 +0530

    votelist is updated

--- src/components/VoteList.js
+++ src/components/VoteList.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { VoteContext } from "../contextFiles/VoteContext";
+import classes from "./VoteList.module.css";
 const VoteList=(props)=>{
     const {votesData,deleteVote}=useContext(VoteContext);
 return(
-    <div>
+    <div className={classes.list}>
      {
         Object.keys(votesData).map((name)=>(
-            <div>
+            <div className={classes.sublist}>
             <h2>{name}</h2>
             <p>Total {votesData[name].length}</p>
             {votesData[name].map((stdName)=>(
