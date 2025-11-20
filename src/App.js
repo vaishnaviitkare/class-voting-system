eeb0dcb554b46896753e80e700568df42f70afc4
vaishnaviitkare <vaishuitkare@gmail.com>
2025-11-20 16:19:11 +0530

    code optimized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { useContext } from "react";
 import {UIContext} from './contextFiles/UIContext';
-import { VoteContext } from "./contextFiles/VoteContext";
 import VoteForm from "./components/VoteForm";
 import './App.css';
 import VoteList from "./components/VoteList";
 import Header from "./Layout/Header";
 function App() {
-  const {openModal,openForm}=useContext(UIContext);
-  const{votesData}=useContext(VoteContext);
-  const totalData=Object.values(votesData).reduce((total,arr)=>{
-      return total=total+arr.length;
-  },0)
+  const {openForm}=useContext(UIContext);
   return (
     <div className="App">
       <Header/>
       <div className="bg-section">
-        <div className="vote">
-      <h2>Total Votes:{totalData}</h2>
-      <button type="button" onClick={openModal}>Add New Vote</button>
-      </div>
       {openForm && <VoteForm/>}
       <VoteList/>
       </div>
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,13 +1,22 @@
+import { useContext,useMemo } from "react";
 import React from "react";
-import logo from "../assets/class.avif"
+import { VoteContext } from "../contextFiles/VoteContext";
+import { UIContext } from "../contextFiles/UIContext";
 import classes from "./Header.module.css";
 const Header=(props)=>{
+    const {openModal}=useContext(UIContext);
+    const{votesData}=useContext(VoteContext);
+  const totalData=useMemo(()=>Object.values(votesData).reduce((total,arr)=>{
+      return total=total+arr.length;
+  },0),[votesData])
     return(
-        <React.Fragment>
        <header className={classes.head}>
         <h1>Class Monitor Vote</h1>
-        </header>
-       </React.Fragment>
+       <div className={classes.rightControls}>
+        <h2>Total Votes: {totalData}</h2>
+        <button onClick={openModal}>Add New Vote</button>
+      </div>
+       </header>
     );
 
 }
